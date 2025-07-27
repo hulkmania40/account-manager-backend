4c242a224264639893f05584d69c7c2658bf81eb
hulkmania40 <ali.mubashir40@gmail.com>
2025-07-28 00:16:36 +0530

    init delete

--- models/invite.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, EmailStr
-from datetime import datetime
-
-class Invite(BaseModel):
-    email: EmailStr
-    token: str
-    created_at: datetime
-    used: bool = False
--- models/user.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, EmailStr, Field
-from typing import Optional
-from bson import ObjectId
-
-class User(BaseModel):
-    email: EmailStr
-    hashed_password: str
-    is_admin: bool = False
